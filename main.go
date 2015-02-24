9c1e6af695540bd63290d4b42dbdf811e5522ef5
Brian Tomlinson <darthlukan@gmail.com>
2015-02-24 18:13:05 +0100

    this is the real initial layout. Do not try to run this now.

--- main.go
+++ main.go
@@ -6,19 +6,71 @@ import (
 
 var (
     ProcessStack []Process
+    ProcessChanStack []chan
 )
 
 type Process struct {
     Pid int
     State string
     Cli string
+    Chan chan
 }
 
 func Fork() {
     // Make a channel for the supplied service arg
-    // return that channel to the stack
+    // append it to ProcessChanStack
+}
+
+func Reboot() {
+    // Reboot system
+    // /sbin/reboot
+}
+
+func Shutdown() {
+    // Shutdown system
+    // /sbin/halt
+}
+
+func Poweroff() {
+    // Poweroff system
+    // /sbin/poweroff
+}
+
+func Start() {
+    // Start Process
+}
+
+func Restart() {
+    // Restart Process
+}
+
+func Stop() {
+    // Stop Process
+}
+
+func GetBaseServices() {
+    // Services required to have a minimally running system
+    // Defined in /etc/lynea/services/base
+}
+
+func DesiredServices() {
+    // Read from /etc/lynea/services/user_defined
+    // and Fork
+}
+
+func StartupSystem() {
+    // PID 1
+    // Socket dir /run
+    // Mount virtual filesystems
+    // Mount real filesystems
+    // Set $HOSTNAME (/proc/sys/kernel/hostname)
+    // Create tmpfiles
+    // Spawn TTYs
+    // Exec (Fork) base services
 }
 
 func main() {
-	fmt.Printf("Hello there!\n")
+    for {
+        // Don't die
+    }
 }
